f3a2633bf0bc67ace2a78f0270254aae3f6c2e2f
Tracy Zhan <TracyZ@invenco.com>
2021-08-04 11:54:10 +1200

    ZQW-0001: Try to upload the third party libraries

---
+++ OpenSSL-Win32/mdc2test.d
@@ -0,0 +1,57 @@
+mdc2test.c
+Note: including file: C:\Program Files (x86)\Windows Kits\10\include\10.0.17134.0\ucrt\string.h
+Note: including file:  C:\Program Files (x86)\Windows Kits\10\include\10.0.17134.0\ucrt\corecrt.h
+Note: including file:   C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Tools\MSVC\14.15.26726\include\vcruntime.h
+Note: including file:    C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Tools\MSVC\14.15.26726\include\sal.h
+Note: including file:     C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Tools\MSVC\14.15.26726\include\concurrencysal.h
+Note: including file:    C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Tools\MSVC\14.15.26726\include\vadefs.h
+Note: including file:  C:\Program Files (x86)\Windows Kits\10\include\10.0.17134.0\ucrt\corecrt_memory.h
+Note: including file:   C:\Program Files (x86)\Windows Kits\10\include\10.0.17134.0\ucrt\corecrt_memcpy_s.h
+Note: including file:    C:\Program Files (x86)\Windows Kits\10\include\10.0.17134.0\ucrt\errno.h
+Note: including file:    C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Tools\MSVC\14.15.26726\include\vcruntime_string.h
+Note: including file:  C:\Program Files (x86)\Windows Kits\10\include\10.0.17134.0\ucrt\corecrt_wstring.h
+Note: including file: D:\CFILES\Projects\WinSSL\openssl-1.1.1c\include\internal/nelem.h
+Note: including file: d:\cfiles\projects\winssl\openssl-1.1.1c\test\testutil.h
+Note: including file:  C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Tools\MSVC\14.15.26726\include\stdarg.h
+Note: including file:  D:\CFILES\Projects\WinSSL\openssl-1.1.1c\include\openssl/err.h
+Note: including file:   D:\CFILES\Projects\WinSSL\openssl-1.1.1c\include\openssl/e_os2.h
+Note: including file:    D:\CFILES\Projects\WinSSL\openssl-1.1.1c\include\openssl/opensslconf.h
+Note: including file:     D:\CFILES\Projects\WinSSL\openssl-1.1.1c\include\openssl/opensslv.h
+Note: including file:    C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Tools\MSVC\14.15.26726\include\stdint.h
+Note: including file:   C:\Program Files (x86)\Windows Kits\10\include\10.0.17134.0\ucrt\stdio.h
+Note: including file:    C:\Program Files (x86)\Windows Kits\10\include\10.0.17134.0\ucrt\corecrt_wstdio.h
+Note: including file:     C:\Program Files (x86)\Windows Kits\10\include\10.0.17134.0\ucrt\corecrt_stdio_config.h
+Note: including file:   C:\Program Files (x86)\Windows Kits\10\include\10.0.17134.0\ucrt\stdlib.h
+Note: including file:    C:\Program Files (x86)\Windows Kits\10\include\10.0.17134.0\ucrt\corecrt_malloc.h
+Note: including file:    C:\Program Files (x86)\Windows Kits\10\include\10.0.17134.0\ucrt\corecrt_search.h
+Note: including file:     C:\Program Files (x86)\Windows Kits\10\include\10.0.17134.0\ucrt\stddef.h
+Note: including file:    C:\Program Files (x86)\Windows Kits\10\include\10.0.17134.0\ucrt\corecrt_wstdlib.h
+Note: including file:    C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Tools\MSVC\14.15.26726\include\limits.h
+Note: including file:   D:\CFILES\Projects\WinSSL\openssl-1.1.1c\include\openssl/ossl_typ.h
+Note: including file:   D:\CFILES\Projects\WinSSL\openssl-1.1.1c\include\openssl/bio.h
+Note: including file:    D:\CFILES\Projects\WinSSL\openssl-1.1.1c\include\openssl/crypto.h
+Note: including file:     C:\Program Files (x86)\Windows Kits\10\include\10.0.17134.0\ucrt\time.h
+Note: including file:      C:\Program Files (x86)\Windows Kits\10\include\10.0.17134.0\ucrt\corecrt_wtime.h
+Note: including file:     D:\CFILES\Projects\WinSSL\openssl-1.1.1c\include\openssl/safestack.h
+Note: including file:      D:\CFILES\Projects\WinSSL\openssl-1.1.1c\include\openssl/stack.h
+Note: including file:     D:\CFILES\Projects\WinSSL\openssl-1.1.1c\include\openssl/opensslconf.h
+Note: including file:     D:\CFILES\Projects\WinSSL\openssl-1.1.1c\include\openssl/cryptoerr.h
+Note: including file:      D:\CFILES\Projects\WinSSL\openssl-1.1.1c\include\openssl/symhacks.h
+Note: including file:    D:\CFILES\Projects\WinSSL\openssl-1.1.1c\include\openssl/bioerr.h
+Note: including file:   D:\CFILES\Projects\WinSSL\openssl-1.1.1c\include\openssl/lhash.h
+Note: including file:  D:\CFILES\Projects\WinSSL\openssl-1.1.1c\include\openssl/bn.h
+Note: including file:   D:\CFILES\Projects\WinSSL\openssl-1.1.1c\include\openssl/opensslconf.h
+Note: including file:   D:\CFILES\Projects\WinSSL\openssl-1.1.1c\include\openssl/bnerr.h
+Note: including file: D:\CFILES\Projects\WinSSL\openssl-1.1.1c\include\openssl/evp.h
+Note: including file:  D:\CFILES\Projects\WinSSL\openssl-1.1.1c\include\openssl/opensslconf.h
+Note: including file:  D:\CFILES\Projects\WinSSL\openssl-1.1.1c\include\openssl/evperr.h
+Note: including file:  D:\CFILES\Projects\WinSSL\openssl-1.1.1c\include\openssl/objects.h
+Note: including file:   D:\CFILES\Projects\WinSSL\openssl-1.1.1c\include\openssl/obj_mac.h
+Note: including file:   D:\CFILES\Projects\WinSSL\openssl-1.1.1c\include\openssl/asn1.h
+Note: including file:    D:\CFILES\Projects\WinSSL\openssl-1.1.1c\include\openssl/opensslconf.h
+Note: including file:    D:\CFILES\Projects\WinSSL\openssl-1.1.1c\include\openssl/asn1err.h
+Note: including file:   D:\CFILES\Projects\WinSSL\openssl-1.1.1c\include\openssl/objectserr.h
+Note: including file: D:\CFILES\Projects\WinSSL\openssl-1.1.1c\include\openssl/mdc2.h
+Note: including file:  D:\CFILES\Projects\WinSSL\openssl-1.1.1c\include\openssl/opensslconf.h
+Note: including file:  D:\CFILES\Projects\WinSSL\openssl-1.1.1c\include\openssl/des.h
+Note: including file:   D:\CFILES\Projects\WinSSL\openssl-1.1.1c\include\openssl/opensslconf.h
